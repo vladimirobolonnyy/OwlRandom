903531ee247e6cb766d6cec3099e68d21e137768
vladimirobolonnyy <obolonnyyv@gmail.com>
2020-05-03 15:57:10 +0300

    test refactoring

--- app/src/test/java/com/obolonnyy/owlrandom/presentation/create/CreateDetailsViewModelTest.kt
+++ app/src/test/java/com/obolonnyy/owlrandom/presentation/create/CreateDetailsViewModelTest.kt
@@ -3,8 +3,6 @@ package com.obolonnyy.owlrandom.presentation.create
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.obolonnyy.owlrandom.database.MainRepository
 import com.obolonnyy.owlrandom.database.MockMainRepo
-import com.obolonnyy.owlrandom.utils.getOrAwaitValue
-import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -22,18 +20,8 @@ internal class CreateDetailsViewModelTest {
     fun before() {
         repo = MockMainRepo()
         viewModel = CreateDetailsViewModel(1, repo)
-
     }
 
-    @Test
-    fun onTitleChanged() {
-        val newTitle = "new title"
-        runBlocking { viewModel.viewState.getOrAwaitValue() }
-        viewModel.onTitleChanged(newTitle)
-        val state = viewModel.viewState.getOrAwaitValue()
-        assert(state is CreateDetailsViewState.Loaded)
-        assert((state as CreateDetailsViewState.Loaded).title == newTitle)
-    }
 
     @Test
     fun onItemChanged() {
--- app/src/test/java/com/obolonnyy/owlrandom/utils/LiveDataUtils.kt
+++ app/src/test/java/com/obolonnyy/owlrandom/utils/LiveDataUtils.kt
@@ -1,7 +1,6 @@
 package com.obolonnyy.owlrandom.utils
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
+import androidx.lifecycle.*
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
@@ -29,4 +28,25 @@ fun <T> LiveData<T>.getOrAwaitValue(
 
     @Suppress("UNCHECKED_CAST")
     return data as T
+}
+
+
+fun <T> LiveData<T>.observeOnce(onChangeHandler: (T) -> Unit) {
+    val observer = OneTimeObserver(handler = onChangeHandler)
+    observe(observer, observer)
+}
+
+class OneTimeObserver<T>(private val handler: (T) -> Unit) : Observer<T>, LifecycleOwner {
+    private val lifecycle = LifecycleRegistry(this)
+
+    init {
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    }
+
+    override fun getLifecycle(): Lifecycle = lifecycle
+
+    override fun onChanged(t: T) {
+        handler(t)
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    }
 }
